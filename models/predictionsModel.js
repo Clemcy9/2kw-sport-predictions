857889b522569937992e57177883583ef237e572
Lewiston001 <lewisemmanuelisang17@gmail.com>
2025-11-05 09:32:01 +0100

    feat: prediction schema added

--- models/predictionsModel.js
+++ models/predictionsModel.js
@@ -1,6 +1,36 @@
 import mongoose, { Types } from "mongoose";
 
-const predictionSchema = new mongoose.Schema();
+const ValueSchema = new mongoose.Schema(
+  {
+    value: { type: String, required: true },
+    abbr: { type: String },
+    odd: { type: mongoose.Schema.Types.Mixed },
+    probability: { type: String, default: null },
+  },
+  { _id: false }
+);
+
+const BetSchema = new mongoose.Schema(
+  {
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    abbr: { type: String },
+    marketType: { type: String },
+    values: [ValueSchema],
+  },
+  { _id: false }
+);
+
+const PredictionSchema = new mongoose.Schema(
+  {
+    eventId: { type: mongoose.Schema.Types.Mixed, required: true },
+    eventName: { type: String, required: true },
+    bets: [BetSchema],
+  },
+  { timestamps: true }
+);
+
+const Prediction = mongoose.model("Prediction", PredictionSchema);
 
 const adminPredictionSchema = new mongoose.Schema(
   {
@@ -20,4 +50,4 @@ const AdminPrediction = mongoose.model(
   "AdminPrediction",
   adminPredictionSchema
 );
-export default AdminPrediction;
+export { Prediction, AdminPrediction };

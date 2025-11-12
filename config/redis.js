fe1a6a6bfde10d4816526b7bed378fb7d48e66d9
clement <clemcy9@gmail.com>
2025-11-12 08:38:45 +0100

    update: still fixing redis config for render

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,12 @@
 import { createClient } from "redis";
-const { REDIS_URL } = process.env;
+const REDIS_URL = process.env.REDIS_URL;
 
 const redisClient = createClient({
   url: REDIS_URL || "redis://localhost:6379",
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
+  // socket: { //use this only for rdss secure connection
+  //   tls: true,
+  //   rejectUnauthorized: false,
+  // },
 });
 
 redisClient.on("error", (err) => console.log("redis client error:", err));

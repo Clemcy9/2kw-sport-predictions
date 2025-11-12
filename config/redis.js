f77474c3dbe30858df0bc0e56b28552ef14454e3
clement <clemcy9@gmail.com>
2025-11-12 08:41:23 +0100

    update: removed fallback redis url to localhost redis, seems to make render redis fail

--- config/redis.js
+++ config/redis.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 const REDIS_URL = process.env.REDIS_URL;
 
 const redisClient = createClient({
-  url: REDIS_URL || "redis://localhost:6379",
+  url: REDIS_URL, //|| "redis://localhost:6379",
   // socket: { //use this only for rdss secure connection
   //   tls: true,
   //   rejectUnauthorized: false,

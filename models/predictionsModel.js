42fc7b5aa128e6f1484beed36990f84a8a1ec85c
clement <clemcy9@gmail.com>
2025-12-13 07:49:18 +0100

    update: made some fields in adminPrediction schema required

--- models/predictionsModel.js
+++ models/predictionsModel.js
@@ -44,9 +44,33 @@ const adminPredictionSchema = new mongoose.Schema(
       type: String,
       enum: ["free_tips", "super_single", "sure_two_odds", "best_prediction"],
     },
-    fixture: { type: mongoose.Schema.Types.Mixed },
-
-    // leagueId: { type: String },
+    bet_type_id: { type: Number, required: true },
+    fixture: {
+      league: {
+        id: Number,
+        name: String,
+        country: String,
+        logo: String,
+        flag: String,
+        season: Number,
+      },
+      fixture: {
+        id: Number,
+        timezone: String,
+        date: Date,
+        timestamp: Number,
+      },
+      teams: mongoose.Schema.Types.Mixed,
+      goals: { home: Number, away: Number },
+      status: mongoose.Schema.Types.Mixed,
+      // home_team: { type: String },
+      // away_team: { type: String },
+      // match_date: Date,
+      // venue: { type: String },
+      // status: { type: String, enum: ["upcoming", "live", "finished"] },
+      // home_score: { type: Number, default: 0 },
+      // away_score: { type: Number, default: 0 },
+    },
     bets: [
       {
         id: Number,
@@ -58,6 +82,7 @@ const adminPredictionSchema = new mongoose.Schema(
             percentage: String, //dynamically calculated
           },
         ],
+        required: true,
       },
     ],
   },
